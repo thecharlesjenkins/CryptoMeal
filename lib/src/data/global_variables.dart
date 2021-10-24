1b95f2e233708c993d7a922d699e68e5f051f608
Choi, Sugju <schoi424@gatech.edu>
2021-10-24 05:00:13 -0400

    Update global_variables.dart

--- lib/src/data/global_variables.dart
+++ lib/src/data/global_variables.dart
@@ -6,16 +6,11 @@ import 'package:crypto_meal/src/data/firestore_database.dart';
 
 class GlobalVariables {
   static final GlobalVariables _globalVariables = GlobalVariables._internal();
-  String user_id = "";
+  static String user_id = "";
 
   final Database database = FirestoreDatabase();
   factory GlobalVariables() {
     return _globalVariables;
   }
-
-  user_id_upload(String user_id) {
-    this.user_id = user_id;
-  }
-
   GlobalVariables._internal();
 }
